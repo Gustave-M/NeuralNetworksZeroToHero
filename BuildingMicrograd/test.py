7696189197b0cc03143dfe0d6571c106ddcaf5f8
g24mouge <gustave.mougenot@imt-atlantique.net>
2025-03-30 16:09:56 +0200

    structures ok now

--- BuildingMicrograd/test.py
+++ BuildingMicrograd/test.py
@@ -1,8 +1,19 @@
 
-import sys
-import os
+#from BuildingMicrograd.structures import Value, Neuron, Layer, NeuralNetwork
+#from BuildingMicrograd.visualization_functions import *
 
 from structures import Value
+from visualization_functions import *
 
-a = Value(-4.0)
-print(a)
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a * b + c
+d = e * 2.0
+f = d.tanh()
+f.backward()
+print(f)
+
+draw_dot(f)
+
+
